866e0ff709647ec14178693acf26324ad2ef5a02
user-ПК\user <barcelonamars@gmail.com>
2018-03-17 11:19:52 +0200

    up4

--- network/src/chat/network/TCPConnection.java
+++ network/src/chat/network/TCPConnection.java
@@ -1,10 +1,8 @@
 package chat.network;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 /**
  * Created by user on 14.03.2018.
@@ -12,12 +10,30 @@ import java.net.Socket;
 public class TCPConnection {
     private final Socket socket;
     private final Thread rxThread;
+    private final TCPConnectionListener eventListener;
     private final BufferedReader in;
     private final BufferedWriter out;
 
-    public  TCPConnection(Socket socket) throws IOException{
+    public  TCPConnection(TCPConnectionListener eventListener, Socket socket) throws IOException{
+        this.eventListener = eventListener;
         this.socket = socket;
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String msg = in.readLine();
+                } catch (IOException e) {
+
+                } finally {
+
+                }
+            }
+        });
+        rxThread.start();
     }
+
+
 }
 
---
+++ network/src/chat/network/TCPConnectionListener.java
@@ -0,0 +1,13 @@
+package chat.network;
+
+/**
+ * Created by user on 17.03.2018.
+ */
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection, String value);
+    void onDisconnect(TCPConnection tcpConnection);
+    void onException(TCPConnection tcpConnection, Exception e);
+
+}
