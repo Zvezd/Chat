c4bc832940bb49e6b0a4f77831d7a76e283051d3
user-ПК\user <barcelonamars@gmail.com>
2018-03-31 11:39:28 +0300

    up8

--- client/src/chat/client/ClientWindow.java
+++ client/src/chat/client/ClientWindow.java
@@ -1,11 +1,18 @@
 package chat.client;
 
+import chat.network.TCPConnection;
+import chat.network.TCPConnectionListener;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * Created by user on 14.03.2018.
  */
-public class ClientWindow extends JFrame {
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
 
     private static final String IP_ADDR = "89.222.249.131";
     private static final int PORT = 8189;
@@ -22,12 +29,59 @@ public class ClientWindow extends JFrame {
         });
     }
 
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("ihor");
+    private final JTextField fieldInput = new JTextField();
+
+    private TCPConnection connection;
+
     private ClientWindow() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
         setAlwaysOnTop(true);
 
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+
+        fieldInput.addActionListener(this);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+
+
         setVisible(true);
+        try {
+            connection = new TCPConnection(this, IP_ADDR, PORT)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+
+    }
+    
 }
